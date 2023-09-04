8311868d44ca0c59001cedf99a98ca41d96a7f64
jedeland <jedeland@live.co.uk>
2023-09-04 10:42:29 +0200

    working as intended, performance is bad though

--- scraper.py
+++ scraper.py
@@ -33,29 +33,35 @@ def get_handbags(base_url_handbag):
     }
     base_page = requests.get(base_url_handbag, headers=headers)
     soup = BeautifulSoup(base_page.content, 'html.parser')
-    print(soup.find_all('img'))
+    # print(soup.find_all('img'))
     for item in soup.find_all('div', {'class': 'col-lg-4 col-md-4 col-sm-4 col-xs-6 catalog-item'}):
         for link in item.find_all('a', href=True):
             # print(f"Follow up link is https://www.momoxfashion.com{link['href']}")
             list_of_items.append(f"https://www.momoxfashion.com{link['href']}")
-    print(list_of_items)
+    # print(list_of_items)
     return list_of_items
 
 
-def get_valid_items(min_b, max_b, min_h, max_h, items_to_search):
-    print()
+def get_valid_items(min_b, max_b, min_h, max_h, item_to_search, result_num):
+    # print()
     headers = { 
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     }
-    base_page = requests.get(base_url_handbag, headers=headers)
+    base_page = requests.get(item_to_search, headers=headers)
     soup = BeautifulSoup(base_page.content, 'html.parser')
+    for values in soup.find_all('div', {'class': 'row new-detail-headline'}):
+        height = values.find('td', {"id": "detail-value-Höhe"})
+        width = values.find('td', {"id": "detail-value-Breite"})
+        if int(height.text.strip().replace("cm", "")) >= min_h and int(height.text.strip().replace("cm", "")) <= max_h and int(width.text.strip().replace("cm", "")) >= min_b and int(height.text.strip().replace("cm", "")) <= max_b:
+            print(f"Item found: {item_to_search}\nHeight: {height.text.strip()}\nWidth: {width.text.strip()}")
+            result_num = result_num + 1
 
 
 if "__main__" == __name__:
-    min_breite = 30
+    min_breite = 34
     max_breite = 41
     min_hohe = 22
-    max_hohe = 25
+    max_hohe = 26
     base_url_handbag = 'https://www.momoxfashion.com/de/damen/accessoires/handtaschen?zustand=Neuwertig_Sehr%20gut'    
     print("Starting scraper!")
     num_pages = get_handbag_pages(base_url_handbag)
@@ -64,11 +70,10 @@ if "__main__" == __name__:
         time.sleep(0.15)
         list_of_links.append(get_handbags(f"{base_url_handbag}&seite={i}"))
     g = 0
+    results_counter = 0
     for items in list_of_links:
-        print(g + 1, items)
-        g = g + 1 
-        #get_valid_items(min_breite, max_breite, min_hohe, max_hohe, items)
-    # input_val = input("1: handbags\n2: backpacks\n")
-    # print(int(input_val) < 3)
-    # if int(input_val) > 0 & int(input_val) < 3:
-    #     target_momox(int(input_val))
+        # print(g + 1, items)
+        for item in items: 
+            # print('Item is ' , item)
+            get_valid_items(min_breite, max_breite, min_hohe, max_hohe, item, results_counter)
+    print("Number of results ", results_counter)
