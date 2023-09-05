a1c8f8edbf33e02b248585f78c61eec7fdc9ad61
jedeland <jedeland@live.co.uk>
2023-09-05 14:14:59 +0200

    works with excell output

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import time
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
 
 # def target_momox(argument_type):
 #     print("Targeting number ", argument_type)
@@ -48,13 +50,24 @@ def get_valid_items(min_b, max_b, min_h, max_h, item_to_search, result_num):
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     }
     base_page = requests.get(item_to_search, headers=headers)
-    soup = BeautifulSoup(base_page.content, 'html.parser')
+    soup = BeautifulSoup(base_page.content, 'lxml')
     for values in soup.find_all('div', {'class': 'row new-detail-headline'}):
         height = values.find('td', {"id": "detail-value-Höhe"})
         width = values.find('td', {"id": "detail-value-Breite"})
         if int(height.text.strip().replace("cm", "")) >= min_h and int(height.text.strip().replace("cm", "")) <= max_h and int(width.text.strip().replace("cm", "")) >= min_b and int(height.text.strip().replace("cm", "")) <= max_b:
-            print(f"Item found: {item_to_search}\nHeight: {height.text.strip()}\nWidth: {width.text.strip()}")
+            
             result_num = result_num + 1
+            # print(f"Df: {df}\nConcat: {pd.concat([df, new_row], ignore_index=True)}",)
+
+            new_row = pd.DataFrame({"name": [item_to_search.split("/p/")[-1]],
+                                    "link": [item_to_search], 
+                                    "height": [height.text.strip().replace("cm", "")],
+                                    "width": [width.text.strip().replace("cm", "")]})
+            return new_row
+            # print(f"Df: {df}\nConcat: {pd.concat([df, new_row], ignore_index=True)}",)
+            # df = pd.concat([df, new_row], ignore_index=True)
+            # print(f"Df: {df}\nConcat: {pd.concat([df, new_row], ignore_index=True)}",)
+            # print(f"Item found: {item_to_search}\nHeight: {height.text.strip()}\nWidth: {width.text.strip()}")
 
 
 if "__main__" == __name__:
@@ -62,7 +75,7 @@ if "__main__" == __name__:
     max_breite = 41
     min_hohe = 22
     max_hohe = 26
-    base_url_handbag = 'https://www.momoxfashion.com/de/damen/accessoires/handtaschen?zustand=Neuwertig_Sehr%20gut'    
+    base_url_handbag = 'https://www.momoxfashion.com/de/damen/accessoires/handtaschen?farbe=bunt_gruen_rot_blau_beige_tuerkis_grau_orange_schwarz_braun_weiss&zustand=Neuwertig_Sehr%20gut&von=6&bis=85'    
     print("Starting scraper!")
     num_pages = get_handbag_pages(base_url_handbag)
     list_of_links = []
@@ -71,9 +84,15 @@ if "__main__" == __name__:
         list_of_links.append(get_handbags(f"{base_url_handbag}&seite={i}"))
     g = 0
     results_counter = 0
+    df = pd.DataFrame(data=None, columns=["name", "link", "height", "width"])
     for items in list_of_links:
         # print(g + 1, items)
         for item in items: 
             # print('Item is ' , item)
-            get_valid_items(min_breite, max_breite, min_hohe, max_hohe, item, results_counter)
+            print(f"Df: {df}")
+            df = pd.concat([df, get_valid_items(min_breite, max_breite, min_hohe, max_hohe, item, results_counter
+                                                )], ignore_index=True)
+            print(f"Concat: {df}")
     print("Number of results ", results_counter)
+    print(df)
+    df.to_excel("scraperoutput.xlsx", engine='xlsxwriter')
